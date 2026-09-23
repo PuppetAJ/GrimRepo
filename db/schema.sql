ca81a59f7fe17452978054234c0997d093ad4676
ajimp <adrianj.web@gmail.com>
2026-09-23 17:29:06 -0400

    Remove the Express and Handlebars app and move the models into the client

--- db/schema.sql
+++
@@ -1,6 +0,0 @@
--- Create database if it doesn't exist, delete it then create if it does
-DROP DATABASE IF EXISTS cards;
-
-CREATE DATABASE cards;
-
-USE cards;
--- models/index.js
+++
@@ -1,8 +0,0 @@
-// Import models
-const Player = require(`./Player`);
-const Card = require('./Card');
-
-// If any associations, declare here
-
-// Export models
-module.exports = { Player, Card };
